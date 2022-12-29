8b210c1bfc58a60cb930a9b98c593f27099375cc
Har0106 <freddiemaron@gmail.com>
2022-12-29 13:58:56 +0530

    displaying previous entries

--- passenger_counter_app/app.js
+++ passenger_counter_app/app.js
@@ -1,6 +1,7 @@
 let incrementButton = document.getElementById('increment-btn')
 let saveButton = document.getElementById('save-btn')
 let countLabel = document.getElementById('count')
+let entriesLabel = document.getElementById('entries')
 let count = 0
 
 incrementButton.addEventListener('click', increment)
@@ -13,6 +14,10 @@ function increment() {
 }
 
 function save() {
+    // display the previous entries
+    let stringCount = " " + count + " -"
+    entriesLabel.innerText += stringCount
+    // setting the count back to 0
     count = 0
     countLabel.innerText = count
 }
