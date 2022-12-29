d45d9e7179e57d1ccd7e1bf556925fd473d7c0dd
Har0106 <freddiemaron@gmail.com>
2022-12-29 13:13:03 +0530

    created save function

--- passenger_counter_app/app.js
+++ passenger_counter_app/app.js
@@ -1,11 +1,18 @@
 let incrementButton = document.getElementById('increment-btn')
+let saveButton = document.getElementById('save-btn')
 let countLabel = document.getElementById('count')
 let count = 0
 
 incrementButton.addEventListener('click', increment)
+saveButton.addEventListener('click', save)
 
 // increasing the count
 function increment() {
     count++
+    countLabel.innerText = count
+}
+
+function save() {
+    count = 0
     countLabel.innerText = count
 }
